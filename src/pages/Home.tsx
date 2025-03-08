f86b6f296fa5c58e16d5b6d78821534d483df878
Alexandre BURCZY <alexandrebzy16@gmail.com>
2025-03-08 17:30:56 +0100

    Add submeta css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,9 @@ const Home: React.FC = () => {
   return (
     <div className="bg-light min-h-screen flex flex-col">
       <main className="flex flex-col items-center justify-center flex-grow">
-        <h2 className="text-secondary text-2xl font-semibold">
-          Welcome to BJJ Flow!
+        <h2 className="text-secondary text-2xl font-semibold text-[3.75rem] leading-[110%] tracking-[-0.075rem] word-spacing-[-0.075rem] m-0 whitespace-pre-wrap pb-8">
+          Welcome to BJJ Flow !
         </h2>
-        <p className="text-dark mt-4 text-center">
-          Your ultimate companion to progress in Brazilian Jiu-Jitsu.
-        </p>
-        <button className="mt-6 bg-secondary hover:bg-primary text-light font-bold py-2 px-4 rounded">
-          Get Started
-        </button>
       </main>
 
       <footer className="bg-dark text-light py-4 text-center text-sm">
